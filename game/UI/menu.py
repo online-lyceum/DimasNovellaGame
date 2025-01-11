11d76f2d0c832d1f4a6edaaed3b025365ed47b25
Lawrence Naumov <prostolawr@gmail.com>
2025-01-12 04:37:27 +0700

    Code refactor

---
+++ game/UI/menu.py
@@ -0,0 +1,65 @@
+import pygame
+from pygame import Surface
+from pygame.event import Event
+
+from game.media_data import big_font
+from game.settings import HEIGHT
+from game.settings import WIDTH
+from game.UI.button import Button
+
+
+class Menu:
+    def __init__(self, title: str, buttons_text: list[str]):
+        self.title = title
+        self.rendered_title = big_font.render(self.title, True, (0, 0, 0))
+        self.space_between = 100
+        self.space_bottom = 100
+        self.button_width = 200
+        self.button_height = 100
+        self.selected = ''
+        self.buttons = []
+        coords = self.calculate_buttons_coordinates(len(buttons_text))
+        for i, button_text in enumerate(buttons_text):
+            self.buttons.append(
+                Button(
+                    button_text,
+                    coords[i],
+                    self.button_width,
+                    self.button_height
+                )
+            )
+
+    @staticmethod
+    def calculate_title_coordinate() -> tuple[int, int]:
+        return 300, 100
+
+    def calculate_buttons_coordinates(self, button_count: int):
+        match button_count:
+            case 1:
+                return [(WIDTH // 2 - self.button_width // 2,
+                         HEIGHT // 2 - self.button_height // 2)]
+
+            case 2:
+                return [
+                    (WIDTH // 2 - self.button_width - self.space_between // 2,
+                     HEIGHT - self.button_height - self.space_bottom),
+                    (WIDTH // 2 + self.button_width + self.space_between // 2,
+                     HEIGHT - self.button_height - self.space_bottom)]
+
+    def draw_title(self, on: Surface):
+        on.blit(self.rendered_title, self.calculate_title_coordinate())
+
+    def draw_buttons(self, on: Surface):
+        for button in self.buttons:
+            button.draw(on)
+
+    def draw(self, on: Surface):
+        self.draw_title(on)
+        self.draw_buttons(on)
+
+    def process_event(self, event: Event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            for button in self.buttons:
+                if button.rect.collidepoint(event.pos):
+                    self.selected = button.text_box.text
+                    return
--- game/scenes/main_menu_scene.py
+++ game/scenes/main_menu_scene.py
@@ -1,81 +1,14 @@
 import pygame
-from pygame.event import Event
 
-from game.media_data import big_font
 from game.scenes.base_scene import BaseSceneController
 from game.scenes.black_scene import BlackSceneController
 from game.settings import FPS
 from game.settings import HEIGHT
 from game.settings import WIDTH
-from game.UI.button import Button
+from game.UI.menu import Menu
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT), flags=pygame.FULLSCREEN)
-
-
-class BaseMenu:
-    def __init__(self, title: str, buttons_text: list[str]):
-        self.title = title
-        self.rendered_title = big_font.render(self.title, True, (0, 0, 0))
-        self.space_between = 100
-        self.space_bottom = 100
-        self.button_width = 200
-        self.button_height = 100
-        self.selected = ''
-        self.buttons = []
-        coords = self.calculate_buttons_coordinates(len(buttons_text))
-        for i, button_text in enumerate(buttons_text):
-            self.buttons.append(
-                Button(
-                    button_text,
-                    coords[i],
-                    self.button_width,
-                    self.button_height
-                )
-            )
-
-    @staticmethod
-    def calculate_title_coordinate() -> tuple[int, int]:
-        return 300, 100
-
-    def calculate_buttons_coordinates(self, button_count: int):
-        match button_count:
-            case 1:
-                return [(WIDTH // 2 - self.button_width // 2,
-                         HEIGHT // 2 - self.button_height // 2)]
-
-            case 2:
-                return [
-                    (WIDTH // 2 - self.button_width - self.space_between // 2,
-                     HEIGHT - self.button_height - self.space_bottom),
-                    (WIDTH // 2 + self.button_width + self.space_between // 2,
-                     HEIGHT - self.button_height - self.space_bottom)]
-
-    def draw_title(self):
-        screen.blit(self.rendered_title, self.calculate_title_coordinate())
-
-    def draw_buttons(self):
-        for button in self.buttons:
-            button.draw(screen)
-
-    def draw(self):
-        self.draw_title()
-        self.draw_buttons()
-
-    def process_event(self, event: Event):
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            for button in self.buttons:
-                if button.rect.collidepoint(event.pos):
-                    self.selected = button.text_box.text
-                    return
-
-    def draw_scene(self):
-        if self.selected == '':
-            return MainMenuSceneController()
-        if self.selected == 'играть':
-            return BlackSceneController()
-        if self.selected == 'пока':
-            return pygame.quit()
 
 
 class MainMenuSceneController(BaseSceneController):
@@ -83,12 +16,12 @@ class MainMenuSceneController(BaseSceneController):
         super().__init__()
 
     def game(self) -> BaseSceneController | None:
-        menu = BaseMenu('привет', ['играть', 'пока'])
+        menu = Menu('привет', ['играть', 'пока'])
 
         while not menu.selected:
             clock = pygame.time.Clock()
             screen.fill((255, 255, 255))
-            menu.draw()
+            menu.draw(screen)
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -98,4 +31,10 @@ class MainMenuSceneController(BaseSceneController):
                     return
 
             clock.tick(FPS)
-        return menu.draw_scene()
+
+        if menu.selected == '':
+            return MainMenuSceneController()
+        if menu.selected == 'играть':
+            return BlackSceneController()
+        if menu.selected == 'пока':
+            return pygame.quit()
