ab12a9467a31a4896b806991c795bb095bfb60b1
Lawrence Naumov <prostolawr@gmail.com>
2025-01-20 04:54:58 +0700

    Edit

--- game/scenes/server_room.py
+++ game/scenes/server_room.py
@@ -1,16 +1,13 @@
 import pygame
 from pygame.event import Event
 
-from game.UI.blackout import Blackout
-from game.UI.character import tioma_o, tioma_b
-from game.UI.backgrounds import mahutov_room
-from game.UI.storytelling import Storytelling
 from game.scenes.base_scene import BaseScene
 from game.settings import FPS
-from game.storys_data import *
+from game.UI.blackout import Blackout
 
 
 secret_key = pygame.K_k
+
 
 def secret_server_room_event_process(event: Event) -> bool:
     return event.type == pygame.KEYDOWN and event.key == secret_key
@@ -18,14 +15,13 @@ def secret_server_room_event_process(event: Event) -> bool:
 
 class ServerRoomScene(BaseScene):
     def game(self) -> BaseScene | None:
-        story_1 = Storytelling(story_black_1)
         blackout = Blackout(secs=1)
         clock = pygame.time.Clock()
         while not blackout.is_end:
             self.screen.fill('red')
             blackout.reverse_draw(self.screen)
             pygame.display.update()
-        while not story_1.is_end:
+        while True:
             self.screen.fill('red')
             pygame.display.update()
 
@@ -35,6 +31,3 @@ class ServerRoomScene(BaseScene):
                     return
 
             clock.tick(FPS)
-
-        from game.scenes.main_menu_scene import MainMenuScene
-        return MainMenuScene(self.screen)
